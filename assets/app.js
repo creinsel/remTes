90ffa5cc21e418b71c5c10d5c0f24de8596fab9d
Cassondra Reinsel <cassiereinsel@gmail.com>
2021-02-17 16:54:47 -0800

    log in name dis

--- assets/app.js
+++ assets/app.js
@@ -6,6 +6,7 @@ let userInfo;
 const emailInput = document.querySelector('.emailIn');
 const pwdInput = document.querySelector('.pwdIn');
 const loginAccont = document.querySelector('.loginAcc');
+const hiYou= document.querySelector('.logInHi');
 
 loginAccont.addEventListener('click', function (e) {
     // Prevent form from submitting
@@ -14,6 +15,18 @@ loginAccont.addEventListener('click', function (e) {
     userInfo = proctors.find(proctor => proctor.email=== emailInput.value)
  
     console.log(userInfo)
+
+    if (userInfo?.password === pwdInput.value){
+        alert('logged in hoe');
+        emailInput.value = pwdInput.value = '';
+
+        hiYou.textContent= `Hello ${userInfo.name}!!!`
+
+    }else{
+        alert('nooooo')
+    }; 
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
     
     
   });

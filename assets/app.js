74f88601963d53f8b8b642fc2e09de2912890aca
Cassondra Reinsel <cassiereinsel@gmail.com>
2021-02-17 16:18:55 -0800

    user info

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,19 @@
 'use strict';
 
-import users from './users.js';
+// import {user} from './users.js';
 
+let userInfo;
+const emailInput = document.querySelector('.emailIn');
+const pwdInput = document.querySelector('.pwdIn');
+const loginAccont = document.querySelector('.loginAcc');
+
+loginAccont.addEventListener('click', function (e) {
+    // Prevent form from submitting
+    e.preventDefault();
+
+    userInfo = proctors.find(proctor => proctor.email=== emailInput.value)
+ 
+    console.log(userInfo)
+    
+    
+  });
